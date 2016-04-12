399eeb493b5e4844d3bcb0458d4c63fa31efafa7
侯 斌 <myvstar@gmail.com>
2016-04-12 09:32:56 +0900

    fix util.test.js

--- test/utils.test.js
+++ test/utils.test.js
@@ -4,26 +4,47 @@ import { describe, it } from 'mocha';
 
 import { isPromise, isAction } from '../src/utils';
 
-describe('test utils', () => {
-  it('test isPromise()', () => {
-    expect(isPromise(Promise.resolve())).to.be.true;
-    expect(isPromise(new Promise((resolve) => resolve(false)))).to.be.true;
-    expect(isPromise(() => true)).to.not.be.true;
-    expect(isPromise({})).to.not.be.true;
+describe('utils', () => {
+  describe('isPromise()', () => {
+    it('Promise.resolve() is a Promise', () => {
+      expect(isPromise(Promise.resolve())).to.be.true;
+    });
+
+    it('new Promise() is a Promise', () => {
+      expect(isPromise(new Promise((resolve) => resolve(false)))).to.be.true;
+    });
+    it('Function is not a Promise', () => {
+      expect(isPromise(() => true)).to.not.be.true;
+    });
+    it('Object is not a Promise', () => {
+      expect(isPromise({})).to.not.be.true;
+    });
   });
 
-  it('test isAction()', () => {
-    expect(isAction({
-      type: 'USER_REQUEST',
-      payload: {},
-    })).to.be.true;
-    expect(isAction({
-      type: '',
-    })).to.not.be.true;
-    expect(isAction(1)).to.not.be.true;
-    expect(isAction(Promise.resolve())).to.not.be.true;
-    expect(isAction({})).to.not.be.true;
-    expect(isAction(() => 1)).to.not.be.true;
+  describe('isAction()', () => {
+    it('Object is an action', () => {
+      expect(isAction({
+        type: 'USER_REQUEST',
+        payload: {},
+      })).to.be.true;
+    });
+    it('Object with an empty type is also an action', () => {
+      expect(isAction({
+        type: '',
+      })).to.not.be.true;
+    });
+    it('Number is not an action', () => {
+      expect(isAction(1)).to.not.be.true;
+    });
+    it('Promise is not an action', () => {
+      expect(isAction(Promise.resolve())).to.not.be.true;
+    });
+    it('Empty object is not an action', () => {
+      expect(isAction({})).to.not.be.true;
+    });
+    it('Function is not an action', () => {
+      expect(isAction(() => 1)).to.not.be.true;
+    });
   });
 });
 /* eslint-enable no-unused-expressions */
